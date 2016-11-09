1ffcd4595729115822905a771e22718c4d9037da
Brian Craig <briancraigok@gmail.com>
2016-11-08 23:58:58 -0300

    Added basic towers & zombies tests

--- app/game/test.ts
+++ app/game/test.ts
@@ -1,8 +1,53 @@
 /// <reference path="../declarations/jasmine.d.ts" />
+import StandardZombie from './enemy/standardzombie'
+import SimpleTower from './tower/simpletower'
 
-describe("test", ()=>{
-  it("should", () => {
-    expect(true).toBe(true);
+describe("testing enemies", ()=>{
+  describe("standard zombie", ()=>{
+    let z = new StandardZombie()
+    it("initial live", () => {
+      expect(z.hearts()).toBe(20);
+    })
+    it("initial speed", () => {
+      expect(z.speed()).toBe(0.7);
+    })
+    it("is alive", () => {
+      expect(z.isAlive()).toBe(true);
+    })
   })
-  
+})
+
+describe("testing towers", ()=>{
+  describe("simple tower", ()=>{
+    let t = new SimpleTower()
+    it("0.2s attack interval", () => {
+      expect(t.attackInterval()).toBe(0.2);
+    })
+    it("attack range", () => {
+      expect(t.range()).toBe(3.5);
+    })
+    it("damage", () => {
+      expect(t.damage()).toBe(5);
+    })
+  })
+})
+
+describe("testing mechanics", ()=>{
+  describe("simple towers vs standard zombie", ()=>{
+    let z = new StandardZombie()
+    let t = new SimpleTower()
+    it("can force attack to zombie", () => {
+      t.attack(z);
+    })
+    it("correct zombie status after one attack", () => {
+      expect(z.hearts()).toBe(15);
+      expect(z.isAlive()).toBe(true);
+    })
+    it("zombie died after 3 attacks", () => {
+      t.attack(z);
+      t.attack(z);
+      t.attack(z);
+      expect(z.isAlive()).toBe(false);
+    })
+  })
 })
